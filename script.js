72c1ce60fa93c7cd44f6ea4b94352ddae458ea41
Mehul Srivastava <srimehul18@gmail.com>
2026-03-14 02:19:20 +0530

    Added error functions and handling error gracefully

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log("Script Started")
+
 const cityInput = document.getElementById('city-input');
 const searchBtn = document.getElementById('search-btn');
 const cityText = document.getElementById("city");
@@ -6,32 +7,50 @@ const tempText = document.getElementById("temp");
 const conditionText = document.getElementById("condition");
 const humidityText = document.getElementById("humidity");
 const windText = document.getElementById("wind");
+
 console.log("All Elements have been selected");
-
 searchBtn.addEventListener("click", Search);
-
-async function Search() {   
+function Search() {
+    clearError()
     console.log("Search button clicked")
-    const city = cityInput.value
-    console.log("City variable:", city);
-    console.log("Searching weather for City: " + city)
+    const city = cityInput.value.trim()
+    console.log("City variable:", city)
+    const apiKey = "Your_API_Key_here"
+    const url =
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     console.log("Before Fetching the weather data")
-    const apiKey = "Your_API_Key_Here"
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;  
-    try{
-    const res = await fetch(url)
-    console.log("After Fetching the weather data")
-    const data = await res.json()
-    cityText.textContent = data.name
-    tempText.textContent = data.main.temp + "°C"
-    conditionText.textContent = data.weather[0].description
-    humidityText.textContent = data.main.humidity + "%"
-    windText.textContent = data.wind.speed + " m/s"
-    console.log("Weather data received:", data)}
-    catch(error){
-        console.error("Error fetching weather data:", error)
-    }
+    fetch(url)
+    .then(res => {
+        if(!res.ok){
+            throw new Error("City not found")
+        }
+        return res.json()
+    })
+    .then(data => {
+        console.log("Weather data received:", data)
+        cityText.textContent = data.name
+        tempText.textContent = data.main.temp + " °C"
+        conditionText.textContent = data.weather[0].description
+        humidityText.textContent = data.main.humidity + " %"
+        windText.textContent = data.wind.speed + " m/s"
+    })
+    .catch(error => {
+        console.error(error)
+        citynotfound()
+    })
 }
 
+function citynotfound() {
+    const cityerr = document.createElement("p")
+    cityerr.id = "cityerr"
+    cityerr.textContent = "City not found. Please try again."
+    cityerr.style.color = "red"
+    document.body.appendChild(cityerr)
+}
 
-
+function clearError() {
+    const oldError = document.getElementById("cityerr")
+    if (oldError) {
+        oldError.remove()
+    }
+}
