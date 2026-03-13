1a37acefa7130c39966503ef6c9981a7386a064f
Mehul Srivastava <srimehul18@gmail.com>
2026-03-14 02:01:06 +0530

    Made the basic functionality

--- script.js
+++ script.js
@@ -10,8 +10,28 @@ console.log("All Elements have been selected");
 
 searchBtn.addEventListener("click", Search);
 
-function Search() {
+async function Search() {   
     console.log("Search button clicked")
     const city = cityInput.value
+    console.log("City variable:", city);
     console.log("Searching weather for City: " + city)
+    console.log("Before Fetching the weather data")
+    const apiKey = "Your_API_Key_Here"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;  
+    try{
+    const res = await fetch(url)
+    console.log("After Fetching the weather data")
+    const data = await res.json()
+    cityText.textContent = data.name
+    tempText.textContent = data.main.temp + "°C"
+    conditionText.textContent = data.weather[0].description
+    humidityText.textContent = data.main.humidity + "%"
+    windText.textContent = data.wind.speed + " m/s"
+    console.log("Weather data received:", data)}
+    catch(error){
+        console.error("Error fetching weather data:", error)
+    }
 }
+
+
+
